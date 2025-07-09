b57bda14f9428de3ec07ee6c9344b95c56875e2b
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-07-09 13:01:13 -0600

    Add level name synonyms

--- config/coinfits.php
+++ config/coinfits.php
@@ -5,5 +5,10 @@ return [
         'KoalaFit'  => ['steps' => 3000,  'minutes' => 20],
         'JaguarFit' => ['steps' => 6000,  'minutes' => 30],
         'HalconFit' => ['steps' => 10000, 'minutes' => 45],
-    ],
-];
+
+        // Soporte para nombres sin el sufijo "Fit"
+        'Koala'  => ['steps' => 3000,  'minutes' => 20],
+        'Jaguar' => ['steps' => 6000,  'minutes' => 30],
+        'Halcon' => ['steps' => 10000, 'minutes' => 45],
+        'Halcón' => ['steps' => 10000, 'minutes' => 45],
+    ],];
